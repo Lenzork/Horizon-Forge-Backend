61a59a48f8eca780f3b114ce140145a3587a9b05
Lenzork <83922068+Lenzork@users.noreply.github.com>
2024-10-27 01:32:39 +0200

    Marked currently unused variables

--- controller/ItemsController.ts
+++ controller/ItemsController.ts
@@ -2,15 +2,15 @@ import type { RouterContext } from "https://deno.land/x/oak@v17.1.2/router.ts";
 import { APIController } from "./APIController.ts";
 
 export class ItemsController extends APIController {
-    handleDelete(context: RouterContext<string>): void {
+    handleDelete(_context: RouterContext<string>): void {
     }
 
-    handleGet(context: RouterContext<string>): void {
+    handleGet(_context: RouterContext<string>): void {
     }
 
-    handlePost(context: RouterContext<string>): void {
+    handlePost(_context: RouterContext<string>): void {
     }
 
-    handlePut(context: RouterContext<string>): void {
+    handlePut(_context: RouterContext<string>): void {
     }
 }
