3afe613d1502ca054ef03afc7c19c72f28b8e6d3
Lenzork <83922068+Lenzork@users.noreply.github.com>
2024-10-27 02:35:25 +0100

    No Default Values. Make it necessary to create an .env file

--- database.ts
+++ database.ts
@@ -1,10 +1,10 @@
 import { Client } from "https://deno.land/x/mysql@v2.12.1/mod.ts";
 
 const client = await new Client().connect({
-    hostname: Deno.env.get("DATABASE_HOST") || "localhost",
-    username: Deno.env.get("DATABASE_USER") || "root",
-    db: Deno.env.get("DATABASE_NAME") || "antropa",
-    password: Deno.env.get("DATABASE_PASSWORD") || "root",
+    hostname: Deno.env.get("DATABASE_HOST"),
+    username: Deno.env.get("DATABASE_USER"),
+    db: Deno.env.get("DATABASE_NAME"),
+    password: Deno.env.get("DATABASE_PASSWORD"),
 });
 
 export default client;
